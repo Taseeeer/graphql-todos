359c5365765a51fa5f88d87f5c16cd3dad8752f3
Taseeeer <syedtaseeershah@gmail.com>
2021-03-24 02:35:47 -0700

    graphql hasura

--- src/index.js
+++ src/index.js
@@ -1,17 +1,53 @@
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import ApolloClient from 'apollo-client';
+import { HttpLink } from 'apollo-link-http';
+import {ApolloProvider, gql} from "@apollo/client";
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+// const client = new ApolloClient({
+//   uri: "https://suitable-anteater-16.hasura.app/v1/graphql",
+// });
+// client.query({
+//   query: gql`
+//     query getTodos {
+//       todos {
+//         done
+//         id
+//         text
+//       }
+//     }
+//   `
+// }).then(data => console.log(data));
+
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: new HttpLink({
+    uri: "https://suitable-anteater-16.hasura.app/v1/graphql", 
+    headers: {
+      'x-hasura-admin-secret': `65pQcKVpTcpCOnDy32irRZUd7QtOVaj0BwgvTue10BH6N0BsGsI6CCmMwNUJ673i`
+   }
+  }),
+});
+
+client.query({
+  query: gql`
+  query getTodos {
+          todos {
+            done
+            id
+            text
+          }
+        }
+  `,
+}).then(data =>  console.log(data));
 
 ReactDOM.render(
   <React.StrictMode>
+  <ApolloProvider client={client}>
     <App />
+  </ApolloProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
